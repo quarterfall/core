c3f57bedd21e7971c755103c1cf98785763bddda
Arjan Egges <arjan@quarterfall.com>
2021-10-27 11:22:55 +0200

    Added IdGenerator and bumped version number.

--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,25 @@ export { courseImages, defaultCourseImage } from "./data/CourseImages";
 export { DatabaseDialect } from "./data/DatabaseDialect";
 export { EditorType } from "./data/EditorType";
 export {
-  AssignmentEventType,
-  BlockEventType,
-  CourseEventType,
-  ModuleEventType,
-  OrganizationEventType,
-  SubmissionEventType,
-  SystemEventType,
-  UserEventType,
+    AssignmentEventType,
+    BlockEventType,
+    CourseEventType,
+    ModuleEventType,
+    OrganizationEventType,
+    SubmissionEventType,
+    SystemEventType,
+    UserEventType,
 } from "./data/EventType";
 export type { EventType } from "./data/EventType";
 export { NotificationType } from "./data/NotificationType";
 export {
-  ProgrammingLanguage,
-  supportedLanguagesSyntaxHighlighting,
-  supportedLanguagesUnitTesting,
+    ProgrammingLanguage,
+    supportedLanguagesSyntaxHighlighting,
+    supportedLanguagesUnitTesting,
 } from "./data/ProgrammingLanguage";
 export { PublicLicenseType } from "./data/PublicLicenseType";
 export { SortingOrder } from "./data/SortingOrder";
 export { StickerType } from "./data/StickerType";
 export type { UnitTest } from "./data/UnitTest";
+export type { AllowedCharacters } from "./IdGenerator";
+export { generateId } from "./IdGenerator";
