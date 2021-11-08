d56d1635331d2e4bb35a9e619cac7627b127a70a
timurcanevci <tim@quarterfall.com>
2021-11-08 11:54:08 +0300

    Updated patterns in validator.

--- src/Validator.ts
+++ src/Validator.ts
@@ -1,7 +1,7 @@
 export const patterns = {
-    email: /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi,
-    youtube: /^(https?:\/\/)?(www\.)?youtu(be|\.be)?(\.com)?\/.+/,
-    github: /(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\.git)(\/?|\#[-\d\w._]+?)$/,
-    time: /^[0-2]?\d:[0-5]\d(:[0-5]\d)?$/,
+    email: /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/i,
+    youtube: /^(https?:\/\/)?(www\.)?youtu(be|\.be)?(\.com)?\/.+/i,
+    github: /(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\.git)(\/?|\#[-\d\w._]+?)$/i,
+    time: /^[0-2]?\d:[0-5]\d(:[0-5]\d)?$/i,
     variable: /^[a-z][a-z_0-9]*$/i,
 };
