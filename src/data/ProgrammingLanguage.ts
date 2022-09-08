ced91267db429112daf5b46e1e4aa0a1fd702361
Tim Evci <tevci93@gmail.com>
2022-09-08 13:41:10 +0300

    Added go and r to supported languages for IO testing.

--- src/data/ProgrammingLanguage.ts
+++ src/data/ProgrammingLanguage.ts
@@ -50,4 +50,6 @@ export const supportedLanguagesIOTesting = [
     ProgrammingLanguage.cpp,
     ProgrammingLanguage.java,
     ProgrammingLanguage.csharp,
+    ProgrammingLanguage.go,
+    ProgrammingLanguage.r,
 ];
