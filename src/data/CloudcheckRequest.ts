3807f77e71325f4174f7c1cae1fe35db4dab65e6
Tim Evci <tevci93@gmail.com>
2022-09-15 11:19:29 +0300

    Added new cloudcheck actions and their options.

--- src/data/CloudcheckRequest.ts
+++ src/data/CloudcheckRequest.ts
@@ -1,14 +1,30 @@
 import { CloudcheckActionType } from "./CloudcheckActionType";
+import { DatabaseDialect } from "./DatabaseDialect";
 import { ProgrammingLanguage } from "./ProgrammingLanguage";
 
 export interface PipelineStepOptions {
     language: ProgrammingLanguage;
+    // For run code action
+    code?: string;
+    inputs?: { input?: string }[];
+    // For run javascript
+    sandbox?: any;
+    external?: string[];
+    expression?: boolean;
     // For git action
     gitUrl?: string;
     gitBranch?: string;
     gitPrivateKey?: string;
     gitPath?: string;
     forceOverrideCache?: boolean;
+    // For webhook action
+    webhookUrl?: string;
+    // For database action
+    databaseDialect?: DatabaseDialect;
+    databaseFileUrl?: string;
+    // For conditional text
+    textOnMatch?: string;
+    textOnMismatch?: string;
 }
 
 export interface PipelineStep {
